aa72f26731af6d139d1dfbb72a87fe3b7e95731d
pawwwline <k.paulieee@gmail.com>
2025-10-09 17:08:40 +0500

    feat(http): add book struct converter

--- internal/converter/book.go
+++ internal/converter/book.go
@@ -1,1 +1,35 @@
 package converter
+
+import (
+	"book-store-api/internal/dto"
+	"book-store-api/internal/models"
+)
+
+func ToBookResponse(b models.Book) dto.BookDTO {
+	return dto.BookDTO{
+		ID:          b.ID.String(),
+		Title:       b.Title,
+		Description: b.Description,
+		Author:      b.Author,
+		ISBN:        b.ISBN,
+		Price:       b.Price,
+	}
+}
+
+func ToBookResponseList(books []models.Book) []dto.BookDTO {
+	resp := make([]dto.BookDTO, 0, len(books))
+	for _, b := range books {
+		resp = append(resp, ToBookResponse(b))
+	}
+	return resp
+}
+
+func ToBookParams(book dto.BookDTO) models.BookParams {
+	return models.BookParams{
+		Title:       book.Title,
+		Description: book.Description,
+		ISBN:        book.ISBN,
+		Price:       book.Price,
+		Author:      book.Author,
+	}
+}
