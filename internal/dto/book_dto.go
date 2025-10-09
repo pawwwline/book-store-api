81669375ddf577aee6f31d8abc9b71f28736b0ad
pawwwline <k.paulieee@gmail.com>
2025-10-09 17:07:38 +0500

    feat(http): add book dto

--- internal/dto/book_dto.go
+++ internal/dto/book_dto.go
@@ -1,1 +1,14 @@
 package dto
+
+import "time"
+
+type BookDTO struct {
+	ID          string    `json:"id"`
+	Title       string    `json:"title"`
+	Author      string    `json:"author"`
+	Description string    `json:"description"`
+	Price       int       `json:"price"`
+	ISBN        string    `json:"isbn"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
