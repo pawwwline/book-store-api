e59844a6f97c270d7854e1f3e9819c3b3c7e094a
pawwwline <k.paulieee@gmail.com>
2025-10-10 14:50:31 +0500

    add main.go init w graceful shutdown

--- cmd/book-store-api/main.go
+++ cmd/book-store-api/main.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	_ "book-store-api/docs"
+	"book-store-api/internal/app"
+	"book-store-api/internal/config"
+)
+
+func main() {
+	cfg, err := config.InitConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	application, err := app.BuildApp(cfg)
+	if err != nil {
+		fmt.Println("Error initializing application", err)
+
+		return
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	if err := application.Run(ctx, cfg.Cache); err != nil {
+		fmt.Println("Error starting application", err)
+
+		return
+	}
+
+	<-ctx.Done()
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := application.Shutdown(shutdownCtx); err != nil {
+
+		return
+	}
 }
